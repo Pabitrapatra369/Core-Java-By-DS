4d4ba4c8dc6381fcd95ba74613374ee683fbb381
Pabitra Patra <pabitrapatra369@gmail.com>
2024-07-15 11:21:39 +0530

    this is commit for class lebel modifier

---
+++ 3.Declaration & Access Modifiers/5.class level modifiers/A.java
@@ -0,0 +1,7 @@
+package pack1;
+
+public class A {
+	public void m1() {
+		System.out.println("this is inside method m1 and class A");
+	}
+}
---
+++ 3.Declaration & Access Modifiers/5.class level modifiers/Test.java
@@ -0,0 +1,7 @@
+private class Test //CE:modifier private not allowed here
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
